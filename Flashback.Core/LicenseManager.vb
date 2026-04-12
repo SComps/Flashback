35d28eeeb904816427c937ce726c8411ad56181c
ScottJ <westdalefarmer@gmail.com>
2026-04-12 04:35:26 -0400

    Replace JSON with manual AOT-safe parser for VB.NET compatibility

--- Flashback.Core/LicenseManager.vb
+++ Flashback.Core/LicenseManager.vb
@@ -1,7 +1,6 @@
 Imports System.IO
 Imports System.Security.Cryptography
 Imports System.Text
-Imports System.Text.Json
 
 Public Class LicenseInfo
     Public Property LicensedTo As String = "FREE NON-COMMERCIAL USE"
@@ -25,9 +24,18 @@ Public Class LicenseManager
         Try
             Dim encryptedData = File.ReadAllBytes(licPath)
             Dim decryptedText As String = Decrypt(encryptedData)
-            Dim info = JsonSerializer.Deserialize(decryptedText, LicenseContext.Default.LicenseInfo)
-            info.IsLicensed = True
-            Return info
+            
+            ' Manual Parsing (AOT Compatible)
+            Dim info As New LicenseInfo()
+            Dim parts = decryptedText.Split("|"c)
+            If parts.Length >= 2 Then
+                info.LicensedTo = parts(0)
+                info.MaxPrinters = Val(parts(1))
+                info.IsLicensed = True
+                Return info
+            End If
+            
+            Throw New Exception("Invalid license format.")
         Catch ex As Exception
             Dim errInfo As New LicenseInfo()
             errInfo.Error = $"{ex.GetType().Name}: {ex.Message}"
@@ -36,13 +44,9 @@ Public Class LicenseManager
     End Function
 
     Public Shared Sub GenerateLicense(userName As String, printerCount As Integer, outPath As String)
-        Dim info As New LicenseInfo With {
-            .LicensedTo = userName,
-            .MaxPrinters = printerCount,
-            .IsLicensed = True
-        }
-        Dim json = JsonSerializer.Serialize(info, LicenseContext.Default.LicenseInfo)
-        Dim encrypted = Encrypt(json)
+        ' Simple Pipe-Delimited Format (AOT Compatible)
+        Dim data = $"{userName}|{printerCount}"
+        Dim encrypted = Encrypt(data)
         File.WriteAllBytes(outPath, encrypted)
     End Sub
 
@@ -76,10 +80,4 @@ Public Class LicenseManager
             End Using
         End Using
     End Function
-End Class
-
-<JsonSourceGenerationOptions(WriteIndented:=True)>
-<JsonSerializable(GetType(LicenseInfo))>
-Public Partial Class LicenseContext
-    Inherits JsonSerializerContext
 End Class
